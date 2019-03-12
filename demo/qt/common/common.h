6d3173dc118d1a31719628c5918b740da324d2d8
jmu201521121021 <863385256@qq.com>
2019-03-12 09:02:46 +0800

    Update common.h

--- demo/qt/common/common.h
+++ demo/qt/common/common.h
@@ -1,16 +1,3 @@
-﻿/**
- * @file       common.h
- * @brief      罗普特数据标注工具公共头文件
- * @details    用于统一包含项目所需的头文件
- * @author     xiaochengliu.prc@foxmail.com
- * @date       2018.01.25
- * @version    V0.1
- * @par Copyright (C):
- *			   罗普特(厦门)科技集团有限公司
- * @par History:
- *  - V0.1     xiaochengliu.prc@foxmail.com         2017.12.25 \n
- *             原型开发 \n
- */
 
 #ifndef __COMMON_H__
 #define __COMMON_H__
